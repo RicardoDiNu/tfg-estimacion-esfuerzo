c5e9b9330552a092dafae44160c282ac17bb0c1e
RicardoDiNu <uo282909@uniovi.es>
2026-04-16 00:21:40 +0200

    chore: bootstrap inicial del proyecto Spring Boot

---
+++ src/main/java/com/uniovi/estimacion/TfgEstimacionEsfuerzoApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.estimacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgEstimacionEsfuerzoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfgEstimacionEsfuerzoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/estimacion/TfgEstimacionEsfuerzoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.estimacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgEstimacionEsfuerzoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
